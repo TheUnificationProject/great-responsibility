8648e6fe6ad4f3d5adc628fb7e4ed34920a54d59
Clément <clementfiesta@gmail.com>
2026-01-24 08:53:09 +0100

    fix: add production server URL to Swagger setup and disable swagger on production mode

--- src/swagger.ts
+++ src/swagger.ts
@@ -3,6 +3,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { name, version } from 'package.json';
 
 export function setupSwagger(app: INestApplication): void {
+  if (process.env.NODE_ENV !== 'development') return;
+
   const options = new DocumentBuilder()
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     .setTitle(name as string)
@@ -10,6 +12,7 @@ export function setupSwagger(app: INestApplication): void {
     .setVersion(version as string)
 
     .addServer('http://localhost:3000', 'Development')
+    .addServer('https://api.clement-fossorier.fr', 'Production')
 
     .build();
 

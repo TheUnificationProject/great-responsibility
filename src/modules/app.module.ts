1a4e3cc0e85bb71dca9b21b9c64b6158a256e2d7
Clément <clementfiesta@gmail.com>
2026-04-12 09:28:30 +0200

    fix: Add MikroOrmMiddlewareModule to AppModule imports

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,4 +1,5 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { MikroOrmMiddlewareModule } from '@mikro-orm/nestjs/mikro-orm-middleware.module';
 import { defineConfig } from '@mikro-orm/postgresql';
 import { AppController } from '@modules/app.controller';
 import { AuthModule } from '@modules/auth/auth.module';
@@ -26,6 +27,7 @@ import { Module } from '@nestjs/common';
     AuthModule,
     SkillsModule,
     FirebaseModule,
+    MikroOrmMiddlewareModule.forRoot(),
   ],
 })
 export class AppModule {}
